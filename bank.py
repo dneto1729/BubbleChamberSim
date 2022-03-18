2b011ad023eeee845f0c42a3974c3663ac2aeb9e
David Neto <57016287+dneto1729@users.noreply.github.com>
2022-03-18 16:03:23 -0500

    Update bank.py
    There seems to be an issue with the kinE values for particles with non-zero mass. Adding in the momentum variables so we can double-check the kinetic energy outside of Geant4.

--- bank.py
+++ bank.py
@@ -19,13 +19,16 @@ bankId       = 2400
 bankname = "bubble"
 
 def define_bank(configuration):
-    
+
     insert_bank_variable(configuration, bankname, "bankid", bankId, "Di", bankname+" bank ID")
     insert_bank_variable(configuration, bankname, "detId", 1, "Di", "paddle number")
     insert_bank_variable(configuration, bankname, "kinE", 2, "Dd", "Kinetic Energy")
     insert_bank_variable(configuration, bankname, "pid", 3, "Di", "particle id")
-    insert_bank_variable(configuration, bankname, "hitn", 99, "Di", "hit number")
+    insert_bank_variable(configuration, bankname, "trackE", 6, "Dd", "Kinetic Energy")
     insert_bank_variable(configuration, bankname, "avg_x", 8, "Dd", "Average X position in the global reference system (in mm)")
     insert_bank_variable(configuration, bankname, "avg_y", 9, "Dd", "Average Y position in the global reference system")
+    insert_bank_variable(configuration, bankname, "px", 14, "Dd", "x component of momentum of the FP (in MeV)")
+    insert_bank_variable(configuration, bankname, "py", 15, "Dd", "y component of momentum of the FP (in MeV)")
+    insert_bank_variable(configuration, bankname, "pz", 16, "Dd", "z component of momentum of the FP (in MeV)")
     insert_bank_variable(configuration, bankname, "procID", 25, "Dd", "Process that created the FP")
-        
+    insert_bank_variable(configuration, bankname, "hitn", 99, "Di", "hit number")
